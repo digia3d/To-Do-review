28609d6f965facf21b54e7187ef1609d1b4b3510
Aleksandra Ujvari <ujvari65@gmail.com>
2022-07-06 19:29:00 +0200

    Fix errors that reviewer ask me to.

--- src/modules/displayData.js
+++ src/modules/displayData.js
@@ -1,24 +1,3 @@
-const data = [
-  {
-    index: 1,
-    description: 'Cook lunch',
-    completed: true,
-  },
-  {
-    index: 2,
-    description: 'Clean house',
-    completed: true,
-  },
-  {
-    index: 3,
-    description: 'Wash dishes',
-    completed: false,
-  },
-  {
-    index: 4,
-    description: 'Feed cat',
-    completed: true,
-  },
-];
+const data = [];
 
 export default data;
