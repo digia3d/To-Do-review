31112d63a91bbdb8fdb20ba21465d0d0ad625493
Aleksandra Ujvari <ujvari65@gmail.com>
2022-07-07 13:52:23 +0200

    Add changes to JS file, and fix funcionality.

--- src/index.js
+++ src/index.js
@@ -1,53 +1,43 @@
-// css files here
+// CSS style
 import './style.css';
-import TodoList from './modules/addTask.js';
-import * as Element from './modules/elements.js';
+import toDoList from './modules/addTask.js';
+import * as Element from './modules/items.js';
 
-const newTask = new TodoList();
+const newTask = new toDoList();
 
-const getCheck = (element) => ` ${
-  element.completed
-    ? `<input type="checkbox" aria-label="${element.index}" data-name="status" name="check" checked>`
+const getCheck = (element) => `${element.completed
+    ? ` <input type= "checkbox" aria-label="${element.index}" data-name="status" name="check" checked>`
     : `<input type="checkbox" aria-label="${element.index}" data-name="status" name="check">`
-} `;
+  }`;
 
 const showTaskItem = (element) => `<div class="list show">
-                ${getCheck(element)}                
-                <p class="taskdescription ${
-  element.completed ? 'strike' : ''
-}">${element.description}</p>
-               <i class="fa fa-ellipsis-v fa-2x menu-icon" aria-label="${
-  element.index
-}"  data-name="edit"></i>
-          </div>`;
+${getCheck(element)}
+<p class="taskdescription ${element.completed ? 'strike' : ''
+  } " > ${element.description}</p>
+<i class="fa fa-ellipsis-v fa-2x menu-icon" aria-label="${element.index}"
+data-name="edit"></i>
+</div>`;
 
 const editDescription = (element) => `<div class="list edit">
-                 ${getCheck(element)} 
-                <input type="text" class="desc" value="${
-  element.description
-}" aria-label ="${element.index}" >
-                <i class="fa fa-trash-o fa-2x" aria-label="${
-  element.index
-}"  data-name="delete"></i>
-          </div>`;
+${getCheck(element)}
+<input type="text" class="desc" value="${element.description}"
+  aria-label ="${element.index}">
+  <i class = "fa fa-trash-o fa-2x" aria-label = "${element.index}" data-name = "delete"></i></div>`;
 
 const refresh = () => {
   const list = newTask.listArray;
   let content = '';
   if (list) {
     list.forEach((element) => {
-      content += `${
-        element.edit ? editDescription(element) : showTaskItem(element)
-      }`;
+      content += `${element.edit ? editDescription(element) : showTaskItem(element)}`;
     });
   }
   Element.listBody.innerHTML = content;
 };
 refresh();
 
-// Event Listeners
 Element.addList.addEventListener('keydown', (e) => {
-  if (e.key === 'Enter') {
+  if (e.code === 'Enter') {
     const val = Element.addList.value;
     if (val) {
       newTask.addTask(val);
@@ -63,8 +53,8 @@ Element.listBody.addEventListener('click', (e) => {
       newTask.setEdit(e.target.ariaLabel);
       refresh();
     } else if (e.target.dataset.name === 'delete') {
-      newTask.removeTask(parseInt(e.target.ariaLabel, 10));
-      refresh();
+newTask.removeTask(parseInt(e.target.ariaLabel, 10));
+refresh();
     }
   }
 });
